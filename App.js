85cf59cdac6b95860aa35d44d27cccf27317cb6d
RavikumarKaipu <135000243+RavikumarKaipu@users.noreply.github.com>
2024-02-03 08:47:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import {Component} from 'react';
+import Counter from './Components/counter';
+
+class App extends Component{
+  render(){
+    return(
+      <Counter />
+    )
+  }
+}
+export default App;
---
+++ Components/counter/index.js
@@ -0,0 +1,44 @@
+import {Component} from 'react';
+import './index.css'
+class Counter extends Component{
+    state={
+        count:0
+    }
+    
+    onIncrement=()=>{
+        this.setState((prevState)=>{
+            return {count: prevState.count + 1 }
+        })
+    }
+    onDecrement=()=>{
+        this.setState((prevState)=>{
+            return {count: prevState.count - 1 }
+        })
+    }
+    render(){
+        const {count}=this.state
+        return(
+
+            <div className='container'>
+                <h1 className='heading'>
+                    Counter
+                </h1>
+                <p className='count'>
+                    {count}
+                    </p>
+                    <div>
+                        <button onClick={this.onIncrement}>
+                            Increase
+                        </button>
+                        <button onClick={this.onDecrement}>
+                            Increase
+                        </button>
+                    </div>
+                    
+            </div>
+        )
+
+        
+    }
+}
+export default Counter
